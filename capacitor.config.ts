6907025930a9c24a7f68c48f31f6385a683cf356
Valentina Zabala <vzabala@mail.austral.edu.ar>
2023-02-14 18:36:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'almproyecto',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
